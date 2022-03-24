bdcbbe1095154e75e01cd901475fe4fc243b44ef
João Ferreira <jo4o.fds@gmail.com>
2022-03-24 01:21:59 -0300

    finaliza busca de especialidades

--- src/components/Agendamento.vue
+++ src/components/Agendamento.vue
@@ -1,34 +1,80 @@
 <template>
   <v-container>
     <v-row class="text-center">
-
       <v-col class="mb-4">
-        <h1 class="display-1 font-weight-bold mb-3">
-          Agendamento de Consulta
-        </h1>
+        <h1 class="display-1 font-weight-bold mb-3">Agendamento de Consulta</h1>
 
         <p class="subheading font-weight-regular">
-          Aqui pode poder realizar o agendamento de suas consultas,
-          <br>por favor preencha os campos abaixo
+          Aqui você pode realizar o agendamento de consultas.<br />
         </p>
       </v-col>
     </v-row>
+
+    <v-row class="d-flex justify-center">
+      <v-col cols="4">
+        <div v-if="specialties.length">
+          <v-select
+            :items="specialties"
+            label="Selecione a especialidade..."
+            item-text="nome"
+            item-value="especialidade_id"
+            v-model="selects.specialtie">
+          </v-select>
+          <div class="d-flex justify-end">
+            <v-btn @click="searchProfessioanls()" small color="success" dark>
+              Buscar
+            </v-btn>
+          </div>
+        </div>
+        <p v-else class="d-flex justify-center subheading font-weight-regular">
+          Carregando dados...
+        </p>
+      </v-col>
+    </v-row>
+
+    <v-overlay :value="overlay">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
   </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'AgendarConsulta',
+export default {
+  name: "AgendarConsulta",
 
-    created() {
-      this.$http.post(`/specialties/list`)
-      .then(res => (this.specialties = res.data))
+  mounted() {
+    this.overlay = true;
+    this.$http
+      .post(`/specialties/list`)
+      .then((res) => {
+        this.specialties = res.data.content;
+      })
+      .then(() => {
+        this.overlay = false;
+      });
+  },
+  data() {
+    return {
+      overlay: false,
+      specialties: [],
+      selects: {
+        specialtie: null
+      }
+    };
+  },
+  methods: {
+    searchProfessioanls() {
 
     },
-    data() {
-      return {
-        specialties: null
-      }
-    }
-  }
+  },
+  computed: {
+
+  },
+};
 </script>
+
+<style scoped>
+.v-list-item__title {
+  text-transform: uppercase;
+}
+</style>
